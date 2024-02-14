e5ab06710dd7a6720fd9c4756c41c5ab31944e1f
Nathan Keidel <78992074+NathanK4261@users.noreply.github.com>
2024-02-13 19:13:25 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,9 @@
-pip3 install pygame
-cd $HOME
-git clone https://github.com/NathanK4261/DodgeTheAsteroids.git
-cd DodgeTheAsteroids
+# Unpack files and remove unnecessary files
 unzip BETA3.5.zip
 rm -dPRrvW __MACOSX || echo "Skipping MacOS file cleanup"
 rm -f BETA3.5.zip
-ls
-cd BETA3.5
-python3 __buildgame__.py
-cd 
-cd Desktop
-chmod +x DodgeTheAsteroids.command
+
+# Build the executable script
+python3 BETA3.5/__buildgame__.py
+mv DodgeTheAsteroids.command ~/Desktop
+chmod +x ~/Desktop/DodgeTheAsteroids.command
